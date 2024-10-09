29dcf3e4f17ce062c5dc96b475ec502863cc3736
Priyank <142205321+cloudbypriyank@users.noreply.github.com>
2024-10-09 19:05:30 +0530

    Create while-loop.cpp

---
+++ while-loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    
+    int n = 1;
+    while (n<=5) {
+        cout << "Number is :"<< n << "\n";
+        n++;
+    }
+    
+    return 0;
+}
